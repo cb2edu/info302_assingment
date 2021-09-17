675b6d84883a22dba7803dcfc81ae17c6c0d3f85
Meghan Moody <mmeghan@uab.edu>
2021-09-17 15:44:21 -0500

    9/17 try

---
+++ Assignment3_Intro2R/R_notes.R
@@ -0,0 +1,11 @@
+# each line has to be run one by one using cmd+enter
+# variable assignment 
+a <- 2 
+a =2
+a
+cat(a)
+#no such thing as scalar object in R; everything is a vector 
+
+#R script is like a bash script and will have the extention of .R or .r 
+#R markdown is a mix or R code and txt 
+
