3d4c2e1a7efd17439ff4d7536af5da958f42f135
Meghan Moody <mmeghan@uab.edu>
2021-12-04 15:24:19 -0600

    done

---
+++ final_problem_set/Problem4_script.R
@@ -0,0 +1,8 @@
+##Test the hypothesis that highly abundant domains will have higher chance of accummulating variations 
+##First run problems 1 and 2 scrips and save output in files 
+##Use an R script to read the files created in problems 3 and 4. 
+##draw a linear regression plot between the abundance in X-axis and number of variations in Y-axis
+##should report the correlation between the two
+
+##run problem 1 script 
+source()
---
+++ final_problem_set/Problem5_script.R
@@ -0,0 +1,17 @@
+##Use Fermi estimation from lecture 1 to estimate the quantity starting from very little knowledge 
+##Describe the question you are trying to answer and how you derived the answer
+
+# Question: How many adults could you fit standing shoulder to shoulder in the state of Texas?
+# Starting Point: Texas is 268,597 sq mi
+
+# The average height for women if 5 ft 4 in and 5 ft 9 in for men in the US
+# So the average height for any adult in the US is 5 ft 6.5 inches 
+# it is suggested that men and women keep their waist circumference no more than half their height 
+# 5 ft 6.5 inches is 66.5 inches so suggested average waist circumfrence is 33.25 inches 
+# The average waist to shoulder ratio is 1.6
+# average shoulder width is 53.2 inches or 4 ft 5 inches 
+# Using the shoulder width I would say the average person takes up about 9 square feet 
+# Texas is 268,597 sq mi or 7.4880546e+12 sq ft 
+# Divide 7.4880546e+12 sq ft by 9 sq ft is 832,000,000,000
+# so 832,000,000,000 can fit standing up next to each other in Texas
+
